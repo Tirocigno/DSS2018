601579d195a28d605309a07b6fe475e00ff96beb
Tirocigno <fede.naldini1@gmail.com>
2018-09-25 08:27:14 +0200

    Implemented readClients method in model

--- DSS2018WFA/Model.cs
+++ DSS2018WFA/Model.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
 
 namespace DSS2018WFA
 {
@@ -16,6 +18,22 @@ namespace DSS2018WFA
             //devo flushare il testo in qualche modo
             for (int i = 0; i < 10; i++)
                 FlushText(this, $"i={i}");
+        }
+
+        public void readClients(string sqLiteConnString)
+        {
+            IDbConnection conn = new SQLiteConnection(sqLiteConnString);
+            conn.Open();
+            IDbCommand com = conn.CreateCommand();
+            string queryText = "select id,nome from clienti";
+            com.CommandText = queryText;
+            IDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                FlushText(this, reader["id"] + " " + reader["nome"]);
+            }
+            reader.Close();
+            conn.Close();
         }
     }
 }
